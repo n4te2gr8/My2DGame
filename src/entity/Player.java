455b350284a139a1525f254b894333300d669db8
n4te2gr8 <honathan06@gmail.com>
2025-06-25 19:00:40 -0700

    Object Interaction
    Implemented object collision and made them interactive so the player can pick them up or expect some reactions.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -1,6 +1,6 @@
 package entity;
 
-import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
@@ -17,6 +17,7 @@ public class Player extends Entity{
 	
 	public final int screenX;
 	public final int screenY;
+	int hasKey = 0;
 	
 	public Player(GamePanel gp, KeyHandler keyH) {
 		this.gp = gp;
@@ -28,6 +29,8 @@ public class Player extends Entity{
 		solidArea = new Rectangle();
 		solidArea.x = 8;
 		solidArea.y = 16;
+		solidAreaDefaultX = solidArea.x;
+		solidAreaDefaultY = solidArea.y;
 		solidArea.width = 32;
 		solidArea.height = 32;
 		
@@ -75,6 +78,10 @@ public class Player extends Entity{
 			collisionOn = false;
 			gp.cChecker.checkTile(this);
 			
+			// CHECK OBJECT COLLISION
+			int objIndex = gp.cChecker.checkObject(this, true);
+			pickUpObject(objIndex);
+			
 			// IF COLLSION IS FALSE, PLAYER CAN MOVE
 			if(collisionOn == false) {
 				switch(direction) {
@@ -104,9 +111,30 @@ public class Player extends Entity{
 				spriteCounter = 0;
 			}
 		}
-		
-
 	}
+	
+	public void pickUpObject(int i) {
+		
+		if(i != 999) {
+			String objectName = gp.obj[i].name;
+			
+			switch(objectName) {
+			case "Key":
+				hasKey++;
+				gp.obj[i] = null;
+				System.out.println("Key: " + hasKey);
+				break;
+			case "Door":
+				if(hasKey > 0) {
+					gp.obj[i] = null;
+					hasKey--;
+				}
+				System.out.println("Key: " + hasKey);
+				break;
+			}
+		}
+	}
+	
 	public void draw(Graphics2D g2) {
 		/*
 		 * g2.setColor(Color.white);
