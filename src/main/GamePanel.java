93643f2b235f2d417b100eeb9c8d9c05ef4468d6
n4te2gr8 <honathan06@gmail.com>
2025-06-28 15:06:07 -0700

    Game State
    Created a game state and pause screen.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -31,7 +31,7 @@ public class GamePanel extends JPanel implements Runnable{
 	int FPS = 60;
 	
 	TileManager tileM = new TileManager(this);
-	KeyHandler keyH = new KeyHandler();
+	KeyHandler keyH = new KeyHandler(this);
 	Sound music = new Sound();
 	Sound se = new Sound();
 	public CollisionChecker cChecker = new CollisionChecker(this);
@@ -42,6 +42,11 @@ public class GamePanel extends JPanel implements Runnable{
 	// ENTITY AND OBJECT
 	public Player player = new Player(this, keyH);
 	public SuperObject obj[] = new SuperObject[10];
+	
+	//GAME STATE
+	public int gameState;
+	public final int playState = 1;
+	public final int pauseState = 2;
 	
 	
 	public GamePanel() {
@@ -55,8 +60,9 @@ public class GamePanel extends JPanel implements Runnable{
 	public void setupGame() {
 		
 		aSetter.setObject();
-		
 		playMusic(0);
+		stopMusic();
+		gameState = playState;
 	}
 	
 	public void startGameThread() {
@@ -125,7 +131,12 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void update() {
-		player.update();
+		if(gameState == playState) {
+			player.update();
+		}
+		if(gameState == pauseState) {
+			// nothing
+		}
 	}
 	
 	public void paintComponent(Graphics g) {
