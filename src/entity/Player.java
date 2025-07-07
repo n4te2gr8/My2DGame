5175e23124c4e97687a30609e2fe77d8a710112f
n4te2gr8 <honathan06@gmail.com>
2025-07-07 16:41:01 -0700

    Health Bar & Death Animation
    Added monster's death animation, a health bar, additional AI, and sound effects.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -215,6 +215,7 @@ public class Player extends Entity{
 				gp.npc[i].speak();
 			}
 			else {
+				gp.playSE(7);
 				attacking = true;
 			}
 		}
@@ -223,6 +224,7 @@ public class Player extends Entity{
 	public void contactMonster(int i) {
 		if(i != 999) {
 			if(invincible == false) {
+				gp.playSE(6);
 				life -= 1;
 				invincible = true;
 			}
@@ -232,11 +234,13 @@ public class Player extends Entity{
 	public void damageMonster(int i) {
 		if(i != 999) {
 			if(gp.monster[i].invincible == false) {
+				gp.playSE(5);
 				gp.monster[i].life -= 1;
 				gp.monster[i].invincible = true;
+				gp.monster[i].damageReaction();
 				
 				if(gp.monster[i].life <= 0 ) {
-					gp.monster[i] = null;
+					gp.monster[i].dying = true;
 				}
 			}
 		}
