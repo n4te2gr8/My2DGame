4bb9f5d25db9c93b358d9eb8e780343c80250c39
n4te2gr8 <honathan06@gmail.com>
2025-07-07 12:28:22 -0700

    Hit Detection
    Implemented the player's attack animation and hit detection to be able to fight and defeat monsters. Plus some minor bug fixes.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.awt.AlphaComposite;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
@@ -13,26 +14,35 @@ import main.UtilityTool;
 public class Entity {
 
 	GamePanel gp;
-	public int worldX, worldY;
-	public int speed;
-	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
-	public String direction = "down";
-	public int spriteCounter = 0;
-	public int spriteNum = 1;
-	public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
-	public int solidAreaDefaultX, solidAreaDefaultY;
-	public boolean collisionOn = false;
-	public int actionLockCounter = 0;
-	public boolean invincible = false;
-	public int invincibleCounter = 0;
-	String dialogues[] = new String[20];
-	int dialogueIndex = 0;
-	public BufferedImage image, image2, image3;
-	public String name;
-	public boolean collision = false;
-	public int type; // 0 = player, 1 = npc, 2 = monster
 	
-	// CHARACTER STATUS
+	public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+	public BufferedImage attackUp1, attackUp2, attackDown1, attackDown2, 
+	attackLeft1, attackLeft2, attackRight1, attackRight2;
+	public BufferedImage image, image2, image3;
+	public Rectangle solidArea = new Rectangle(0, 0, 48, 48);
+	public Rectangle attackArea = new Rectangle(0, 0, 0, 0);
+	public int solidAreaDefaultX, solidAreaDefaultY;
+	public boolean collision = false;
+	String dialogues[] = new String[20];
+	
+	// STATE
+	public int worldX, worldY;
+	public String direction = "down";
+	public int spriteNum = 1;
+	int dialogueIndex = 0;
+	public boolean collisionOn = false;
+	public boolean invincible = false;
+	boolean attacking = false;
+	
+	// COUNTER
+	public int spriteCounter = 0;
+	public int actionLockCounter = 0;
+	public int invincibleCounter = 0;
+	
+	// CHARACTER ATTRIBUTES
+	public int type; // 0 = player, 1 = npc, 2 = monster
+	public String name;
+	public int speed;
 	public int maxLife;
 	public int life;
 	
@@ -109,6 +119,14 @@ public class Entity {
 			}
 			spriteCounter = 0;
 		}
+		
+		if(invincible == true) {
+			invincibleCounter++;
+			if(invincibleCounter > 40) {
+				invincible = false;
+				invincibleCounter = 0;
+			}
+		}
 	}
 	public void draw(Graphics2D g2) {
 		BufferedImage image = null;
@@ -123,49 +141,40 @@ public class Entity {
 		   worldY - gp.tileSize < gp.player.worldY + gp.player.screenY) {
 			switch(direction) {
 			case "up":
-				if(spriteNum == 1) {
-					image = up1;
-				}
-				if(spriteNum == 2) {
-					image = up2;
-				}
+				if(spriteNum == 1) {image = up1;}
+				if(spriteNum == 2) {image = up2;}
 				break;
 			case "down":
-				if(spriteNum == 1) {
-					image = down1;
-				}
-				if(spriteNum == 2) {
-					image = down2;
-				}
+				if(spriteNum == 1) {image = down1;}
+				if(spriteNum == 2) {image = down2;}
 				break;
 			case "left":
-				if(spriteNum == 1) {
-					image = left1;
-				}
-				if(spriteNum == 2) {
-					image = left2;
-				}
+				if(spriteNum == 1) {image = left1;}
+				if(spriteNum == 2) {image = left2;}
 				break;
 			case "right":
-				if(spriteNum == 1) {
-					image = right1;
-				}
-				if(spriteNum == 2) {
-					image = right2;
-				}
+				if(spriteNum == 1) {image = right1;}
+				if(spriteNum == 2) {image = right2;}
 				break;
 			}
+			
+			if(invincible == true) {
+				g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.4f));
+			}
+			
 			g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+			
+			g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
 		}
 	}
 	
-	public BufferedImage setup(String imagePath) {
+	public BufferedImage setup(String imagePath, int width, int height) {
 		UtilityTool uTool = new UtilityTool();
 		BufferedImage image = null;
 		
 		try {
 			image = ImageIO.read(getClass().getResourceAsStream(imagePath + ".png"));
-			image = uTool.scaleImage(image,  gp.tileSize, gp.tileSize);
+			image = uTool.scaleImage(image, width, height);
 		}
 		catch(IOException e) {
 			e.printStackTrace();
