e22e8c3c3ff45bc408d862a48eb88b92d2ac7686
n4te2gr8 <honathan06@gmail.com>
2025-07-09 12:43:44 -0700

    Scrolling Message, Leveling Up, Damage Calculation
    Updated battle system by utilizing characters' attack and defense values.  Displayed battle messages and handled leveling up.

--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -85,6 +85,7 @@ public class EventHandler {
 			gp.player.attackCanceled = true;
 			gp.ui.currentDialogue = "You drink the water.\nYour life has been recovered.";
 			gp.player.life = gp.player.maxLife;
+			gp.aSetter.setMonster();
 		}
 	}
 }
