a218d2d674aca2d59d7f454548115f5485b7a799
n4te2gr8 <honathan06@gmail.com>
2025-07-11 22:31:04 -0700

    Monster Projectiles and Mana
    Implemented monster projectiles and handled projectile resources (Mana/Ammo) so you can shoot only a limited number of projectiles.

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -53,6 +53,7 @@ public class Entity {
 	public int life;
 	public int maxMana;
 	public int mana;
+	public int ammo;
 	public int level;
 	public int strength;
 	public int dexterity;
@@ -125,16 +126,7 @@ public class Entity {
 		boolean contactPlayer = gp.cChecker.checkPlayer(this);
 		
 		if(this.type == type_monster && contactPlayer == true) {
-			if(gp.player.invincible == false) {
-				// we can give damage
-				gp.playSE(6);
-				int damage = attack - gp.player.defense;
-				if(damage < 0) {
-					damage = 0;
-				}
-				gp.player.life -= damage;
-				gp.player.invincible = true;
-			}
+			damagePlayer(attack);
 		}
 		
 		// IF COLLSION IS FALSE, PLAYER CAN MOVE
@@ -173,7 +165,24 @@ public class Entity {
 				invincibleCounter = 0;
 			}
 		}
+		if(shotAvailableCounter < 30) {
+			shotAvailableCounter++;
+		}
 	}
+	
+	public void damagePlayer(int attack) {
+		if(gp.player.invincible == false) {
+			// we can give damage
+			gp.playSE(6);
+			int damage = attack - gp.player.defense;
+			if(damage < 0) {
+				damage = 0;
+			}
+			gp.player.life -= damage;
+			gp.player.invincible = true;
+		}
+	}
+	
 	public void draw(Graphics2D g2) {
 		BufferedImage image = null;
 		
--- src/main/UI.java
+++ src/main/UI.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 
 import object.OBJ_Heart;
 import object.OBJ_Key;
+import object.OBJ_ManaCrystal;
 import entity.Entity;
 
 public class UI {
@@ -21,7 +22,7 @@ public class UI {
 	GamePanel gp;
 	Graphics2D g2;
 	Font editUndo, purisaB;
-	BufferedImage heart_full, heart_half, heart_blank;
+	BufferedImage heart_full, heart_half, heart_blank, crystal_full, crystal_blank;
 	public boolean messageOn = false;
 //	public String message = "";
 //	int messageCounter = 0;
@@ -54,6 +55,9 @@ public class UI {
 		heart_full = heart.image;
 		heart_half = heart.image2;
 		heart_blank = heart.image3;
+		Entity crystal = new OBJ_ManaCrystal(gp);
+		crystal_full = crystal.image;
+		crystal_blank = crystal.image2;
 	}
 	
 	public void addMessage(String text) {
@@ -125,6 +129,25 @@ public class UI {
 			x += gp.tileSize;
 		}
 		
+		// DRAW MAX MANA
+		x = gp.tileSize/2-5;
+		y = (int)(gp.tileSize*1.5);
+		i = 0;
+		while(i < gp.player.maxMana) {
+			g2.drawImage(crystal_blank, x, y, null);
+			i++;
+			x += 35;
+		}
+		
+		// DRAW MANA
+		x = gp.tileSize/2-5;
+		y = (int)(gp.tileSize*1.5);
+		i = 0;
+		while(i < gp.player.mana) {
+			g2.drawImage(crystal_full, x, y, null);
+			i++;
+			x += 35;
+		}
 	}
 	
 	public void drawMessage() {
@@ -296,6 +319,8 @@ public class UI {
 		textY += lineHeight;
 		g2.drawString("Life", textX, textY);
 		textY += lineHeight;
+		g2.drawString("Mana", textX, textY);
+		textY += lineHeight;
 		g2.drawString("Strength", textX, textY);
 		textY += lineHeight;
 		g2.drawString("Dexterity", textX, textY);
@@ -309,7 +334,7 @@ public class UI {
 		g2.drawString("Next Level", textX, textY);
 		textY += lineHeight;
 		g2.drawString("Coin", textX, textY);
-		textY += lineHeight + 20;
+		textY += lineHeight + 10;
 		g2.drawString("Weapon", textX, textY);
 		textY += lineHeight + 15;
 		g2.drawString("Shield", textX, textY);
@@ -327,6 +352,11 @@ public class UI {
 		textY += lineHeight;
 		
 		value = String.valueOf(gp.player.life + "/" + gp.player.maxLife);
+		textX = getXforAlignToRightText(value, tailX);
+		g2.drawString(value, textX, textY);
+		textY += lineHeight;
+		
+		value = String.valueOf(gp.player.mana + "/" + gp.player.maxMana);
 		textX = getXforAlignToRightText(value, tailX);
 		g2.drawString(value, textX, textY);
 		textY += lineHeight;
@@ -366,9 +396,9 @@ public class UI {
 		g2.drawString(value, textX, textY);
 		textY += lineHeight;
 		
-		g2.drawImage(gp.player.currentWeapon.down1, tailX - gp.tileSize, textY - 14, null);
+		g2.drawImage(gp.player.currentWeapon.down1, tailX - gp.tileSize, textY - 24, null);
 		textY += gp.tileSize;
-		g2.drawImage(gp.player.currentShield.down1, tailX - gp.tileSize, textY - 14, null);
+		g2.drawImage(gp.player.currentShield.down1, tailX - gp.tileSize, textY - 24, null);
 	}
 	public void drawInventory() {
 		int frameX = gp.tileSize*9;
