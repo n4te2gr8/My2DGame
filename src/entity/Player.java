dcc49efd205e0ea5d9949cfff99b282035808e71
n4te2gr8 <honathan06@gmail.com>
2025-07-12 10:36:02 -0700

    Item Drops
    Implemented an item drop function so monsters randomly drop an item when you kill them.

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -226,6 +226,13 @@ public class Player extends Entity{
 		if(shotAvailableCounter < 30) {
 			shotAvailableCounter++;
 		}
+		
+		if(life > maxLife) {
+			life = maxLife;
+		}
+		if(mana > maxMana) {
+			mana = maxMana;
+		}
 	}
 	
 	public void attacking() {
@@ -275,18 +282,29 @@ public class Player extends Entity{
 	public void pickUpObject(int i) {
 		
 		if(i != 999) {
-			String text;
 			
-			if(inventory.size() != maxInventorySize) {
-				inventory.add(gp.obj[i]);
-				gp.playSE(1);
-				text = "Got a " + gp.obj[i].name + "!";
+			//PICKUP ONLY ITEMS
+			if(gp.obj[i].type == type_pickupOnly) {
+				gp.obj[i].use(this);
+				gp.obj[i] = null;
 			}
-			else {
-				text = "You cannot carry any more!";
+			
+			// INVENTORY ITEMS
+			else {			
+				String text;
+				
+				if(inventory.size() != maxInventorySize) {
+					inventory.add(gp.obj[i]);
+					gp.playSE(1);
+					text = "Got a " + gp.obj[i].name + "!";
+				}
+				else {
+					text = "You cannot carry any more!";
+				}
+				gp.ui.addMessage(text);
+				gp.obj[i] = null;
+				
 			}
-			gp.ui.addMessage(text);
-			gp.obj[i] = null;
 		}
 	}
 	
