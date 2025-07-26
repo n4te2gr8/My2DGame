fef13c8855c7f458f1114e33ae23ab14d08c4c93
n4te2gr8 <honathan06@gmail.com>
2025-07-25 18:12:04 -0700

    Map Screen and Minimap
    Added a map screen and minimap function.

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -18,6 +18,7 @@ import ai.PathFinder;
 import entity.Entity;
 import entity.Player;
 import environment.EnvironmentManager;
+import tile.Map;
 import tile.TileManager;
 import tile_interactive.InteractiveTile;
 
@@ -61,6 +62,7 @@ public class GamePanel extends JPanel implements Runnable{
 	Config config = new Config(this);
 	public PathFinder pFinder = new PathFinder(this);
 	EnvironmentManager eManager = new EnvironmentManager(this);
+	Map map = new Map(this);
 	Thread gameThread;
 	
 	// ENTITY AND OBJECT
@@ -86,7 +88,7 @@ public class GamePanel extends JPanel implements Runnable{
 	public final int transitionState = 7;
 	public final int tradeState = 8;
 	public final int sleepState = 9;
-	
+	public final int mapState = 10;
 	
 	public GamePanel() {
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
@@ -272,6 +274,10 @@ public class GamePanel extends JPanel implements Runnable{
 		if(gameState == titleState) {
 			ui.draw(g2);
 		}
+		// MAP SCREEN
+		else if(gameState == mapState) {
+			map.drawFullMapScreen(g2);
+		}
 		else {
 			//TILE
 			tileM.draw(g2);
@@ -337,6 +343,9 @@ public class GamePanel extends JPanel implements Runnable{
 			
 			// ENVIRONMENT
 			eManager.draw(g2);
+			
+			// MINI MAP
+			map.drawMiniMap(g2);
 			
 			// UI
 			ui.draw(g2);
